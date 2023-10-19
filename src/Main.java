9b8959ae5722d58ea7d1d06d6c9bd378ab442174
Sergey-2023ch <639j1mo64aaz@mail.ru>
2023-10-19 19:12:59 +0300

    new_branch commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("You're good!");
         System.out.println("It is so beautiful!");
-        System.out.println("These things doesn't attract me");
-        System.out.println("This is the last commit");
+        int a = 10;
     }
 }
